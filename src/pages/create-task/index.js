2d78a4db8444b5bdeaa95a13c1d5771b3f7c90f4
Daramola Kehinde Joseph <daramolakehinde339@gmail.com>
2023-11-23 14:37:04 +0100

    multiple select dropdown

--- src/pages/create-task/index.js
+++ src/pages/create-task/index.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
-// import { initFlowbite } from 'flowbite';
+import Multiselect from "multiselect-react-dropdown";
+
 const CreateTask = () => {
 
-    // useEffect(() => {
-    //     initFlowbite();
-    // })
     const [isChecked, setIsChecked] = useState(false);
+    const [options, setOptions] = useState(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']);
     return (<div className="e_pages">
-        <div className="bg-white rounded-lg">
+        <div className="bg-white rounded-lg pb-7">
             <div className="w-full grid grid-cols-2">
                 <div className="w-full  pr-5">
                     <div className="w-full px-3 pt-4">
@@ -34,7 +33,7 @@ const CreateTask = () => {
                                 <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description:</label>
                                 <textarea id="message" rows="2" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#103d15] focus:border-[#103d15] block w-full p-2.5" placeholder="Write your thoughts here..."></textarea>
                             </div>
-                            <div class="flex items-start mb-3">
+                            <div class="flex items-start mb-4">
 
                                 <div class="flex items-center h-5">
                                     <input
@@ -51,23 +50,32 @@ const CreateTask = () => {
                             {
                                 isChecked ?
                                     <div className="mb-6">
-                                        <p className="text-sm font-medium text-gray-900 dark:text-gray-300">
-                                            How often can you commit to the task?
-                                        </p>
-                                        <div class="flex items-center">
+                                        <div className="flex justify-start items-center mb-3">
+                                            <div className="h-[2px] w-[50px] rounded-lg bg-[#103d15] mr-2"></div>
+                                            <p className="text-sm  font-medium text-gray-900 dark:text-gray-300">
+                                                How often can you commit to the task?
+                                            </p>
+                                        </div>
+                                        <div class="flex items-center mb-2">
+                                            <label for="link-checkbox" class=" mr-2 text-sm font-medium text-gray-900 dark:text-gray-300">Every day</label>
                                             <input id="link-checkbox"
                                                 type="checkbox"
                                                 value=""
                                                 class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-[#103d15]"
                                             />
-                                            <label for="link-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Everyday.</label>
+
                                         </div>
                                         <div>
-
+                                            <label for="link-checkbox" class=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Every week:</label>
+                                            <Multiselect
+                                                isObject={false}
+                                                options={options}
+                                            />
                                         </div>
                                     </div>
 
                                     :
+
                                     <div className="mb-6">
                                         <label
                                             for="firstname"
@@ -97,9 +105,7 @@ const CreateTask = () => {
                     </div>
 
                 </div>
-                <div className="w-full  ">
-                    <img src="../../assets/img/bayem/ARJPAWK.jpg" className="h-full w-full rounded-tr-lg rounded-br-lg" alt="farmlogo" />
-                </div>
+
             </div>
         </div>
     </div>);
