f1478acd09f0a6b475cf0e4aa3a3bdcfb7f61e25
Daramola Kehinde Joseph <daramolakehinde339@gmail.com>
2023-12-15 16:02:26 +0100

    updates on weather forecast

--- src/pages/weather-forecast/index.js
+++ src/pages/weather-forecast/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const WeatherForecast = () => {
     const [data, setData] = useState({});
-    const [location, setLocation] = useState("");
+    const [location, setLocation] = useState("lagos");
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3029869ef5b648a92f4e15108b824a81}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=3029869ef5b648a92f4e15108b824a81`
 
 
     const searchLocation = (event) => {
@@ -17,40 +17,56 @@ const WeatherForecast = () => {
             setLocation("");
         }
     }
+
+    useEffect(() => {
+        fetchUpdate();
+    }, [])
+
+    const fetchUpdate = () => {
+        axios.get(url).then((response) => {
+            setData(response.data);
+            console.log(response.data);
+        })
+    }
+
+    const progressStyle = {
+        width: '50%'
+    }
     return (
         <div className="e_pages relative max-w-[1100px]">
-            <div className="weather_bg ">
-                <div className='w-full h-[300px] z-40 relative inset-0 bg-[rgba(0,0,0,0.4)]'>
+
+            <div className="weather_bg h-[300px] relative">
+                <div className='w-full h-full  z-40 relative inset-0 bg-[rgba(0,0,0,0.4)]'>
                     <div className=' pt-10 px-3'>
                         <div className='flex justify-between items-start'>
                             <div>
                                 <div className='location mb-2'>
                                     <p className='text-white font-[500] text-xl'>
-                                        Dallas
+                                        {data.name}
                                     </p>
                                 </div>
 
                                 <div className='temp mb-2'>
                                     <p className='text-white text-6xl font-[600]'>
-                                        60°F
+                                        {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
                                     </p>
                                 </div>
 
                                 <div className='description relative w-full '>
                                     <p className='  text-white text-xl font-[500]'>
-                                        Clouds
+                                        {data.main ? <p className='text-white font-[500] text-xl'>{data.weather[0].main}</p> : null}
                                     </p>
                                 </div>
                             </div>
 
                             <div className='flex justify-between items-center'>
                                 <div className='mr-2'>
-                                    <input type='text' 
-                                       value={location}
-                                       onChange={event => setLocation(event.target.value)}
-                                       onKeyDown={searchLocation}
-                                       placeholder='Enter Loacation'
-                                       className=' rounded-3xl focus:ring-0 h-[35px] text-white bg-[rgba(255,255,255,0.2)] border-white border placeholder:text-white'
+                                    <input type='text'
+                                        value={location}
+                                        onChange={event => setLocation(event.target.value)}
+                                        onKeyDown={searchLocation}
+                                        placeholder='Enter Loacation'
+                                        className=' rounded-3xl focus:ring-0 h-[35px] text-white bg-[rgba(255,255,255,0.2)] border-white border placeholder:text-white'
                                     />
                                 </div>
                                 <div className='w-[40px] h-[40px] rounded-full flex justify-center items-center bg-[rgba(255,255,255,0.2)]'>
@@ -60,24 +76,43 @@ const WeatherForecast = () => {
 
                         </div>
 
-                        <div className='flex py-1 bg-[rgba(255,255,255,0.2)] absolute bottom-3 rounded-md w-auto px-8'>
-                            <div className="feels mr-4">
-                                <p className='text-white font-[500] text-xl'>65°F</p>
-                                <p className='text-white text-sm'>Feels Like</p>
-                            </div>
+                        {
+                            data.name != undefined &&
+                            <div className='flex py-1 bg-[rgba(255,255,255,0.2)] absolute bottom-3 rounded-md w-auto px-8'>
+                                <div className="feels mr-4">
+                                    {data.main ? <p className='text-white font-[500] text-xl'>{data.main.feels_like}°F</p> : null}
+                                    <p className='text-white text-sm'>Feels Like</p>
+                                </div>
 
-                            <div className='humidity mr-4'>
-                                <p className='text-white font-[500] text-xl'>20%</p>
-                                <p className='text-white text-sm'>Humidity</p>
-                            </div>
+                                <div className='humidity mr-4'>
+                                    {data.main ? <p className='text-white font-[500] text-xl'>{data.main.humidity}%</p> : null}
+                                    <p className='text-white text-sm'>Humidity</p>
+                                </div>
 
-                            <div className='wind'>
-                                <p className='text-white font-[500] text-base'>12 MPH</p>
-                                <p className='text-white text-sm'>Wind</p>
+                                <div className='wind'>
+                                    {data.main ? <p className='text-white font-[500] text-xl'>{data.wind.speed}°F</p> : null}
+                                    <p className='text-white text-sm'>Wind Speed</p>
+                                </div>
                             </div>
-                        </div>
+                        }
+
                     </div>
                 </div>
+            </div>
+
+            <div className='border h-[500px]'>
+                <div className='pt-4'>
+                    <p className='font-[500]'>
+                        Diseases and Pest on Crops
+                    </p>
+                </div>
+                <div>
+                    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                        <div class="bg-[#ff9c40] text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full h-[13px]" style={progressStyle}> 45%</div>
+                    </div>
+                    
+                </div>
+
             </div>
         </div>
     );
