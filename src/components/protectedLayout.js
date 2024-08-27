cc845f2167cb029361917ebf2833eaee8b7c9846
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2024-08-27 09:03:31 +0100

    Update protectedLayout.js

--- src/components/protectedLayout.js
+++ src/components/protectedLayout.js
@@ -1,28 +1,13 @@
-import { Outlet, redirect, useNavigate } from "react-router-dom";
-// import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "./navbar";
 import Sidebar from './sidebar';
 const ProtectedLayout = () => {
-    // const navigate = useNavigate();
-    // useEffect(() => {
-    //     authUser();
-    // })
     
-    // const authUser = () => {
-    //     const user = localStorage.getItem('tokenId');
-    //     if (user) {
-    //        return;
-    //     } else {
-    //         return navigate("/login");
-    //     }
-    // }
     return (
         <div>
             <Outlet />
             <Navbar />
             <Sidebar />
-
-            
         </div>
     )
 }
